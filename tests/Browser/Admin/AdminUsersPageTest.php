1f948c09ba2c288ee3645f39b51d1047ebbf55a9
stillwaiting <bgvozdev@gmail.com>
2024-06-12 22:33:44 +1000

    refactor: merge EditorRoleTest with AdminRoleTest

--- tests/Browser/Admin/AdminUsersPageTest.php
+++ tests/Browser/Admin/AdminUsersPageTest.php
@@ -9,11 +9,25 @@ use Tests\Browser\Components\AdminMenu;
 use Tests\Browser\Components\LoginForm;
 use Tests\DuskTestCase;
 
-class AdminRoleTest extends DuskTestCase
+class AdminUsersPageTest extends DuskTestCase
 {
-    /**
-     * A basic browser test example.
-     */
+    public function testEditorCanNotAccessAdminUserPage(): void
+    {
+        $this->browse(function (Browser $browser) {
+            (new LoginForm($browser))->logIn('editor_russian@editor.com', '123456');
+
+            $menu = new AdminMenu($browser);
+            $menu
+                ->waitTillLoaded()
+                ->assertUsersMenuMissing();
+
+            $browser->visit($menu->getUsersMenuLink());
+
+            $browser->waitForText('ACCESS DENIED', 10);
+            $browser->assertTitle('Forbidden');
+        });
+    }
+
     public function testAdminCanAccessUsersPage(): void
     {
         $this->browse(function (Browser $browser) {
